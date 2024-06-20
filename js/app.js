c9a8ac7dac8220e829b46d7fbcbe8010e19c077f
Yevhen K <zhekan89@gmail.com>
2024-06-20 10:09:48 +0300

    save

--- js/app.js
+++ js/app.js
@@ -14,6 +14,13 @@
             document.documentElement.classList.add(className);
         }));
     }
+    function addLoadedClass() {
+        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
     function getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
@@ -5228,6 +5235,7 @@
     da.init();
     window["FLS"] = false;
     isWebp();
+    addLoadedClass();
     menuInit();
     formFieldsInit({
         viewPass: false,
